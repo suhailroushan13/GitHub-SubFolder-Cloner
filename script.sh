6d23984da3aefe7d7d8d979005d1c710ae52c02a
suhailroushan13 <suhailroushan13@gmail.com>
2022-09-04 11:34:35 +0000

    Files_Added✅

--- script.sh
+++ script.sh
@@ -11,27 +11,16 @@ folderName="Git-SubFolder-Cloner"
 branch=""
 master="master"
 main="main"
-
-
 echo -e "${YELLOW}************************************"
 echo -e "${GREEN}${bold}Welcome to GitHub SubFolder Cloner"
 echo -e "By @suhailroushan13 "
 echo -e "${YELLOW}************************************${ENDCOLOR}"
-# echo -e "\n"
-
 read -p "${bold}Enter the GitHub Repository URL: " url
 echo -e "${CYAN}${bold}GitHub URL is: $url\n${ENDCOLOR}"
-
 read -p "${bold}Enter the SubFolder Name From Repository: " folder
 echo -e "${MAGENTA}${bold}Cloning Folder Name is: $folder\n${ENDCOLOR}"
-
-
-
-
 read -p "${bold}Enter the Branch Name of the Repo Example (master/main) : " branch
 echo -e "${YELLOW}${bold}Branch Name is: $branch\n${ENDCOLOR}\n"
-
-
 mkdir $folderName
 cd $folderName
 git init 
@@ -39,5 +28,4 @@ git remote add -f origin $url
 git config core.sparseCheckout true
 git sparse-checkout set $folder
 git pull origin $branch
-
 echo -e "${GREEN}${bold}Cloning the SubFolder from GitHub is Completed inside The Folder\n: $folderName${ENDCOLOR}"
